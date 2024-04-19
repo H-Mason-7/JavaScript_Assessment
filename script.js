f66cc6640a7c75301a5916b7fc01cc633dd0e903
H-Mason-7 <hallekat101@outlook.com>
2024-04-19 01:15:16 -0400

    first attempt commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const firstName = 'Halle';
+const lastName = 'Mason';
+const age = 19;
+
+alert('My name is ' + firstName + ' ' + lastName);
+alert('I am ' + age + ' years old');
+
+console.log('I am very sorry for how late this assignment is.');
